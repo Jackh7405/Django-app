0c660a7524c135587785471dc3140970f22f1207
Jackh7405 <jack.haston103@gmail.com>
2025-10-21 14:15:39 +0000

    Added basic project structure

---
+++ itreporting/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path
+
+app_name = 'itreporting'
+
+urlpatterns = [
+
+    path('', views.home, name = 'home'),
+    path('contact/', views.contact, name = 'contact'),
+    path('about/', views.about, name = 'about'),
+
+]
