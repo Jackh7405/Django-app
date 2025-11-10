92959f54befc4a9fa3cacf0079f0809ddaca6b15
Jackh7405 <jack.haston103@gmail.com>
2025-11-10 11:30:45 +0000

    finished work

--- itreporting/urls.py
+++ itreporting/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 app_name = 'itreporting'
 
 urlpatterns = [
@@ -9,4 +9,10 @@ urlpatterns = [
     path('contact/', views.contact, name = 'contact'),
     path('about/', views.about, name = 'about'),
     path('home/', views.home, name = 'home-alias'),
+    path('report/', PostListView.as_view(), name = 'report'),
+    path('issues/<int:pk>', PostDetailView.as_view(), name = 'issue-detail'),
+    path('issues/new', PostCreateView.as_view(), name = 'issue-create'),
+    path('issues/<int:pk>/update/', PostUpdateView.as_view(), name = 'issue-update'),
+    path('issues/<int:pk>/delete/', PostDeleteView.as_view(), name = 'issue-delete'),
+    path('issue/<str:username>', UserPostListView.as_view(), name = 'user-issues'),
 ]
