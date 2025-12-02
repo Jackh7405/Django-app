b8b626bfbd35c3d987a36bae1f051474a0f868db
Jackh7405 <jack.haston103@gmail.com>
2025-12-02 14:39:54 +0000

    chnaged secret key

--- itapps/settings.py
+++ itapps/settings.py
@@ -86,7 +86,7 @@ import os
 from decouple import config
 
 # Security settings
-SECRET_KEY = os.environ.get('SECRET_KEY', config('SECRET_KEY', default='Sk1b1d1R1zz67'))
+SECRET_KEY = os.environ.get('SECRET_KEY', 'Sk1b1d1R1zz67')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # Database configuration
